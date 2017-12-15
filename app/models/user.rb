b3891d1e56d3798213f388c0def26957d0ca4e34
Leandro Viturino <leandroviturino.si@gmail.com>
2017-12-15 03:17:08 +0000

    Add user secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ActiveRecord::Base
+  #Relaciona a classe user com artigos, atribuindo 1..n
   has_many :artigos
+  #Antes de salvar coloca a string do e-mail em minusculo
   before_save { self.email = email.downcase }
+  #Validação dos dados
   validates :username, presence: true, 
             uniqueness: {case_sensitive: false}, 
             length: { minimum: 3, maximum: 25 }
+  #Regex para validar e-mail
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
             uniqueness: { case_sensitive: false },
             length: { maximum: 105 },
             format: { with: VALID_EMAIL_REGEX }
+  #Verifica a segurança da senha enviada
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214194958) do
+ActiveRecord::Schema.define(version: 20171215030817) do
 
   create_table "artigos", force: :cascade do |t|
     t.string   "titulo"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171214194958) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
